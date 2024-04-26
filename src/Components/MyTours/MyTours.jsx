f21dafd7d11a0fd4d3014bacb3d937e38591bb7b
shariarsultanfahim <shariarsfahim@gmail.com>
2024-04-26 16:10:17 +0600

    users added data is added as a table with update and delete button

--- src/Components/MyTours/MyTours.jsx
+++ src/Components/MyTours/MyTours.jsx
@@ -25,10 +25,15 @@ const MyTours = () => {
         })
     });
 
-
+    const handleUpdate = (id) =>{
+        console.log(id)
+    }
+    const handleDelete = (id) =>{
+        console.log(id)
+    }
 
     return (
-        <div className="container mx-auto min-h-screen my-10">
+        <div className="mx-auto min-h-screen my-10">
             <h1 className="pb-6 text-center text-2xl md:text-4xl lg:text-5xl">Tours Added by <span className="font-medium md:font-bold text-green-800">You</span></h1>
             <div>
             <div className="overflow-x-auto">
@@ -36,38 +41,51 @@ const MyTours = () => {
                     {/* head */}
                     <thead>
                     <tr>
-                        <th>Tour Name</th>
-                        <th>Details</th>
+                        <th>Tours</th>
+                        <th className="hidden md:table-cell">Avarage Cost</th>
+                        <th className="hidden md:table-cell">Visitors per year</th>
+                        <th className="hidden md:table-cell">Season</th>
                         <th>Update</th>
                         <th>Delete</th>
                     </tr>
                     </thead>
                     <tbody>
                     {/* row 1 */}
-                    <tr>
+                    {
+                        myList.map((list, idx) => 
+                        <tr key={idx}>
                         <td>
                         <div className="flex items-center gap-3">
                             <div className="avatar">
                             <div className="mask mask-squircle w-12 h-12">
-                                <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                                <img src={list.photo} alt="Tour Photo" />
                             </div>
                             </div>
                             <div>
-                            <div className="font-bold">Hart Hagerty</div>
-                            <div className="text-sm opacity-50">United States</div>
+                            <div className="font-bold">{list.name}</div>
+                            <div className="text-sm opacity-50">{list.country}</div>
                             </div>
                         </div>
                         </td>
-                        <td>
-                        Zemlak, Daniel and Leannon
-                        <br/>
-                        <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                        <td className="hidden md:table-cell">
+                            {list.avarageCost}
                         </td>
-                        <td>Purple</td>
+                        <td className="hidden md:table-cell">
+                            {list.visitorsPerYear}
+                        </td>
+                        <td className="hidden md:table-cell">
+                            {list.season}
+                        </td>
+                        <td>
+                            <button onClick={()=>{handleUpdate(list._id)}} className="btn btn-xs text-white bg-green-800 hover:bg-green-900">Update</button>
+                        </td>
                         <th>
-                        <button className="btn btn-ghost btn-xs">details</button>
+                        <button onClick={()=>{handleDelete(list._id)}} className="btn btn-xs text-white bg-green-800 hover:bg-green-900">Delete</button>
                         </th>
-                    </tr>
+                        </tr>
+                    )
+                    }
+                    
                     </tbody>
                 </table>
                 </div>
